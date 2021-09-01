c5823cdb2b8c5f16c9b4fb368a4944f756773d96
Jaspal Singh <jaspalsinghpersonal9@gmail.com>
2021-09-01 17:30:54 +0530

    Customized Slider Input

--- src/App.js
+++ src/App.js
@@ -18,12 +18,19 @@ function App() {
   const [songInfo, setSongInfo] = useState({
     currentTime: 0,
     duration: 0,
+    animationPercentage: 0,
   });
   const [libraryStatus, setLibraryStatus] = useState(false);
   function timeUpdateHandler(e) {
     const currentTime = e.target.currentTime;
     const duration = e.target.duration;
-    setSongInfo({ ...songInfo, currentTime, duration });
+    //Calculate Percentage
+    const roundedCurrent = Math.round(currentTime);
+    const roundedDuration = Math.round(duration);
+    const animationPercentage = Math.round(
+      (roundedCurrent / roundedDuration) * 100
+    );
+    setSongInfo({ ...songInfo, currentTime, duration, animationPercentage });
   }
   return (
     <div className="App">
